f6b9fca82f5e74d42e810bcf2d3e8f61a2740aef
Gaurav Hegade <neph005@protonmail.com>
2023-11-08 17:36:14 +0530

    Add page titles and links to resume and GitHub repositories

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,15 +1,23 @@
+<svelte:head>
+    <title>About Me</title>
+</svelte:head>
+
 <h1>About Gaurav Hegade</h1>
 <p>
-  Hello, I am a developer from India with a focus on Linux, System
-  Administration, and Cloud Architecture Development and Deployment. My journey
-  in the world of technology has led me to explore a diverse range of areas, and
-  I'm excited to share a bit about my expertise and interests. Linux has always
-  held a special place in my heart. The open-source nature of the Linux
-  operating system and the customization and automation it offers have always
-  fascinated me. In the web development domain, I have been working on building
-  applications with Svelte and SvelteKit. I like their reactive and
-  component-based approach to building web applications. Furthermore, I have a
-  quite decent grasp in Python, having worked with popular web frameworks like
-  Django and Flask.
+    Hello, I am a developer from India with a focus on Linux, System
+    Administration, and Cloud Architecture Development and Deployment. My
+    journey in the world of technology has led me to explore a diverse range of
+    areas, and I'm excited to share a bit about my expertise and interests.
+    Linux has always held a special place in my heart. The open-source nature of
+    the Linux operating system and the customization and automation it offers
+    have always fascinated me. In the web development domain, I have been
+    working on building applications with Svelte and SvelteKit. I like their
+    reactive and component-based approach to building web applications.
+    Furthermore, I have a quite decent grasp in Python, having worked with
+    popular web frameworks like Django and Flask.
 </p>
-<a href="/">Home</a>
+
+<p>
+    check out my resume
+    <a href="https://maglit.me/myresume">here</a>
+</p>
--- src/routes/projects-repos/+page.svelte
+++ src/routes/projects-repos/+page.svelte
@@ -1,34 +1,51 @@
 <script>
-  import { browser } from "$app/environment";
+    import { browser } from "$app/environment";
 
-  /**
-   * @type {any[]}
-   */
-  let repos = [];
+    /**
+     * @type {any[]}
+     */
+    let repos = [];
+    let repo_url = "https://www.github.com/";
 
-  async function getRepos() {
-    try {
-      const response = await fetch(
-        `https://api.github.com/users/gauravhegade/repos`
-      );
-      const data = await response.json();
-      // console.log(data);
-      repos = data;
-    } catch (error) {
-      console.error(error);
+    async function getRepos() {
+        try {
+            const response = await fetch(
+                `https://api.github.com/users/gauravhegade/repos`
+            );
+            const data = await response.json();
+            // console.log(data); // log data if needed later on
+            repos = data;
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 </script>
 
-<p>Projects go here</p>
+<svelte:head>
+    <title>Projects and Repositories</title>
+</svelte:head>
+
 {#if browser}
-  {#await getRepos()}
-    <p>Loading Data Please Wait...</p>
-  {:then}
-    {#each repos as repo}
-      <p>{repo.name}</p>
-    {/each}
-  {:catch error}
-    <p>{error.message}</p>
-  {/await}
+    {#await getRepos()}
+        <p>Loading Data Please Wait...</p>
+    {:then}
+        <p>
+            Number of repositories, excluding private repositories: {repos.length}
+        </p>
+
+        {#each repos as repo}
+            <p>
+                Title:
+                <a
+                    href="{repo_url}{repo.full_name}"
+                    style="text-decoration: none;">{repo.name}</a
+                >
+                {#if repo.description ?? (repo.description = "Not yet added")}
+                    <br />Description: {repo.description}
+                {/if}
+            </p>
+        {/each}
+    {:catch error}
+        <p>{error.message}</p>
+    {/await}
 {/if}
