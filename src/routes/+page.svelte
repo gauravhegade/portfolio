b3b2d2b660c6a397ee50ee6290482df47c9d1cbc
Gaurav Hegade <neph005@protonmail.com>
2023-11-08 17:38:37 +0530

    Add TODO list  update page title  style the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
+<script>
+    let todo = `TODO NEXT: <br> 1. Implement a blog page. <br> 2. Add a visitor count to the website <br> 3. ADD A FEATURE WHERE PEOPLE CAN ADD SOME TOKEN OF APPRECIATION(may change idk what this is lmao) WHEN THEY VISIT THE WEBSITE AS A MARK OF BEING VISITED`;
+</script>
+
+<svelte:head>
+    <title>Welcome to my Portfolio</title>
+</svelte:head>
+
 <h1>Welcome to my portfolio website</h1>
 
 <p>Go to my <a href="/about">about page</a> to learn more about me</p>
+
+<p id="todo">
+    {@html todo.toLowerCase()}
+</p>
+
+<style>
+    #todo {
+        font-size: 3em;
+        font-weight: 900;
+    }
+</style>
