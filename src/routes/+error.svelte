dafe60fc49ca49b812b9cb052411632c63133737
Gaurav Hegade <neph005@protonmail.com>
2023-12-29 19:45:01 +0530

    Add new types and utility function, and create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+// @ts-nocheck
+
+    import { page } from '$app/stores';
+
+    const emojis = {
+        404: '🧱',
+        420: '🫠',
+        500: '💥',
+    };
+</script>
+
+<h1>
+    <span>
+        {emojis[$page.status] ?? emojis[500]}
+    </span>
+
+    {$page.status}
+    {$page.error.message}
+</h1>
